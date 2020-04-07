2fa498aee6fa29e716936c95ece39b7bb286468b
Shinya Uryu <suika1127@gmail.com>
2020-04-07 18:10:44 +0900

    Try figure upload

---
+++ 04-ggplot2_in_japanese.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+
+p <- 
+  ggplot(iris, aes(Sepal.Length, Petal.Width)) +
+  geom_point()
+ggsave(filename = "ggplot_out.png",
+       p)
+
+p_jp <- 
+  p +
+  xlab("萼片長") +
+  ylab("花弁幅") +
+  theme_gray(base_family = "Osaka")
+ggsave(filename = "ggplot_out_jp.png",
+       p_jp)

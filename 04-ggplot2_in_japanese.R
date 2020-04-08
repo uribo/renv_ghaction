e2c151636b551a9766b0917ce17f5510f25f4a59
Shinya Uryu <uryu.shinya@nies.go.jp>
2020-04-08 16:34:01 +0900

    Test

--- 04-ggplot2_in_japanese.R
+++ 04-ggplot2_in_japanese.R
@@ -4,18 +4,19 @@ extrafont::loadfonts()
 p <- 
   ggplot(iris) +
   aes(Sepal.Length, Petal.Width) +
-  geom_point()
-# ggsave(filename = "ggplot_out.png",
-#        p)
+  geom_point() +
+  theme_bw()
+ggsave(filename = "ggplot_out.png",
+       p)
 
-p_jp <-
-  p +
-  xlab("萼片長") +
-  ylab("花弁幅") +
-  theme_gray(base_family = "Osaka")
-warnings()
-# png("ggplot_out_jp.png", width = 2100, height = 2100, units = "px", res = 300)
-# p_jp
-# dev.off()
-ggsave(filename = "ggplot_out_jp.png",
-       p_jp)
+# p_jp <-
+#   p +
+#   xlab("萼片長") +
+#   ylab("花弁幅") +
+#   theme_gray(base_family = "Osaka")
+# warnings()
+# # png("ggplot_out_jp.png", width = 2100, height = 2100, units = "px", res = 300)
+# # p_jp
+# # dev.off()
+# ggsave(filename = "ggplot_out_jp.png",
+#        p_jp)
